af8f02e060e444efc0c317a296ad47b35a1c30d1
Abdullah Khawaja <hax.3xploit@gmail.com>
2023-04-05 01:01:28 +0500

    Update sql_writer.py

--- sql_writer.py
+++ sql_writer.py
@@ -6,7 +6,8 @@ from colorama import Fore, Back, Style
 
 folder_path = input("Please enter folder location: ")
 sql_folder = input("Please enter output folder location: ")
-code_extensions = [".py", ".php", ".java", ".cpp", ".c", ".h", ".js", ".html", ".css", ".rb", ".pl", ".sh", ".m", ".swift", ".kt", ".scala", ".cs", ".vb", ".aspx", ".jsp", ".xml", ".json", ".yaml", ".yml", ".ini", ".conf", ".md", ".txt"]
+code_extensions = [".aspx", ".au", ".bak", ".baml", ".c", ".cache", ".cd", ".coffee", ".Compressed Files", ".cpp", ".cs", ".cshtml", ".csproj", ".css", ".Databases", ".disco", ".discomap", ".Dockerfile", ".Docs", ".dtproj", ".edmx", ".edr", ".ent", ".env", ".eot", ".ep", ".epgz", ".epz", ".er1", ".fd", ".fdt", ".fdx", ".feature", ".filters", ".flow", ".fmb", ".fnc", ".fnt", ".FOT", ".GID", ".glb", ".gmk", ".go", ".gradle", ".Graphics", ".gyp", ".gypi", ".h", ".hlf", ".hpp", ".htc", ".html", ".java", ".jfif", ".jfm", ".jks", ".jmconfig", ".jmx", ".jnlp", ".jpa", ".jpr", ".js", ".json", ".json5", ".jsonc", ".jsp", ".jst", ".jsx", ".ko", ".kt", ".launch", ".layout", ".less", ".m", ".m4", ".manifest", ".map", ".markdown", ".Master", ".mdx", ".Misc", ".mobileprovision", ".modulemap", ".node", ".nupkg", ".nuspec", ".patch", ".pbd", ".pbl", ".pblx", ".pbr", ".pbt", ".pbtx", ".pcf", ".pdb", ".php", ".pkb", ".pks", ".plist", ".pp", ".prc", ".prefs", ".projdata", ".properties", ".props", ".psd1", ".pubxml", ".py", ".pyc", ".R", ".resources", ".resx", ".rnn", ".rpt", ".SCC", ".scss", ".Setups", ".sh", ".shfbproj", ".sitemap", ".sl", ".sln", ".snap", ".snk", ".so", ".spc", ".spl", ".srd", ".sru", ".storyboard", ".strings", ".suo", ".svc", ".svcinfo", ".svclog", ".svcmap", ".swift", ".t", ".targets", ".tdr", ".template", ".toml", ".tps", ".transform", ".trdx", ".trg", ".trx", ".ts", ".tsx", ".tt", ".ttf", ".tv", ".UDF", ".user", ".vb", ".vbproj", ".vcproj", ".vcxproj", ".vdproj", ".Videos", ".VIW", ".vspscc", ".vw", ".webinfo", ".wixproj", ".woff", ".woff2", ".wsdl", ".xaml", ".xcconfig", ".xcscheme", ".xdt", ".xib", ".xlsm", ".xml", ".xsd", ".xsl", ".xslt", ".xss", ".xsx", ".yaml", ".yml", ".zip"]
+
 
 
 def get_code_files(folder_path):
@@ -22,13 +23,14 @@ def get_code_files(folder_path):
 
 def write_sql_query(file_path, sql_folder):
     try:
-        with open(file_path, "r") as f:
+        with open(file_path, "r", encoding="utf-8", errors="ignore") as f:
             source_code = f.read()
             source_code = source_code.replace("'", "").replace('"', '')
             filename = os.path.basename(file_path)
-            sql_query = f"INSERT INTO cs_fp (filename, source_code) VALUES ('{filename}', '{source_code}');"
+            file_extension = os.path.splitext(filename)[1][1:]
+            sql_query = f"INSERT INTO {file_extension} (filename, source_code) VALUES ('{filename}', '{source_code}');"
             sql_file_path = os.path.join(sql_folder, f"{filename}.sql")
-            with open(sql_file_path, "w", encoding="utf8") as sql_file:
+            with open(sql_file_path, "w", encoding="utf8", errors='ignore') as sql_file:
                 sql_file.write(sql_query)
                 print(f"{Fore.GREEN}[+]{Fore.RESET} Wrote SQL query for {filename}")
     except Exception as e:
